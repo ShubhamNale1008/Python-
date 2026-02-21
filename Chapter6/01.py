9b5cc12c4a38eeaa3443e87f5d92e5ee3465a93a
Shubham Nale <shubhamnale1008@gmail.com>
2026-02-21 18:25:22 +0530

    F1

---
+++ Chapter6/01.py
@@ -0,0 +1,50 @@
+# if else
+
+driver=int(input("Enter F1 driver no:"))
+
+if driver==1:
+    print("Lando Norris")
+elif driver==3:
+    print("Max Verstappen")
+elif driver==16:
+    print("Charles Leclerc")
+elif driver==55:
+    print("Carlos Sainz")
+elif driver==63:
+    print("George Russell")
+elif driver==14:
+    print("Fernando Alonso")
+elif driver==10:
+    print("Pierre Gasly")
+elif driver==11:
+    print("Sergio Perez")
+elif driver==18:
+    print("Lance Stroll")
+elif driver==22:
+    print("Yuki Tsunoda")
+elif driver==4:
+    print("Liam Lawson")
+elif driver==24:
+    print("Zhou Guanyu")
+elif driver==81:
+    print("Oscar Piastri")
+elif driver==6:
+    print("Isack Hadjar")
+elif driver==23:
+    print("Alexander Albon")
+elif driver==18:
+    print("Lance Stroll")
+elif driver==5:
+    print("Gabriel Bortoleto")
+elif driver==27:
+    print("Nico Hülkenberg")
+elif driver==77:
+    print("Valtteri Bottas")
+elif driver==31:
+    print("Esteban Ocon")
+elif driver==12:
+    print("Kimi Anntonelli")
+elif driver==47:
+    print("Arvid Lindblad")
+else:
+    print("No such driver exists")
